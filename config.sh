8c9efcf3904a271cb8caedf53291f71e8b3e3de1
Anil Madhavapeddy <anil@recoil.org>
2014-05-16 08:10:19 +0100

    clarify versions

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@ LOG_REPO=../opam-bulk-logs
 LOG_REPO_URL=git@github.com:avsm/opam-bulk-logs
 DOCKER_REPO=../docker-opam
 DOCKER_REPO_URL=git@github.com:avsm/docker-opam
-VERSIONS="4.01 4.02"
+VERSIONS="4.01.0 4.02.0"
 BASE_IMAGES="ubuntu-trusty"

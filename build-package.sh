3a361b50ef0e2c63b4d3a6a1aba34de2b57d8395
Anil Madhavapeddy <anil@recoil.org>
2016-07-07 15:56:34 +0100

    build-package for opam

---
+++ build-package.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Build an OPAM package for a given OS/OCaml version combination.
+
+LOGDIR=$1
+shift
+PKG=$1
+shift
+
+if [ "$PKG" = "" ]; then
+  echo "Usage: $0 <logdir> <pkg name>"
+  exit 1
+fi
+
+META=$LOGDIR/${PKG}
+mkdir -p ${META}
+opam install --show-actions $PKG > ${META}/actions 2>&1
+opam show $PKG > ${META}/info 2>&1
+starttime=`date +%s`
+echo ${starttime} > ${META}/start_time
+opam depext -uiv $PKG > ${META}/stdout 2>${META}/stderr
+RES=$?
+endtime=`date +%s`
+echo ${endtime} > ${META}/end_time
+difftime=$(($endtime - $starttime))
+echo $difftime > ${META}/build_time
+exit $RES

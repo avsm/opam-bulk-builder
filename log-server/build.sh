5795572546c45c40c368f6417884c05e3c11235d
Anil Madhavapeddy <anil@recoil.org>
2016-07-14 09:48:17 +0100

    remove log server as its built in avsm/opam-log-server on github now

--- log-server/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ocamlbuild -use-ocamlfind opam_log_server.native
--- log-server/db.mli
+++
@@ -1,7 +0,0 @@
-type t = 
-  < add : string -> string Lwt.t; delete : string -> bool Lwt.t;
-    get : string -> string option Lwt.t >
-
-val file : string -> t
-val memory : unit -> t
-

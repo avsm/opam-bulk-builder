275a100e7870ccf66a934b06c7849d1c72333231
Anil Madhavapeddy <anil@recoil.org>
2016-07-14 22:00:36 +0100

    use jsontee

--- build-package.sh
+++ build-package.sh
@@ -15,10 +15,11 @@ opam install --show-actions $PKG > ${LOGDIR}/actions 2>&1
 opam show $PKG > ${LOGDIR}/info 2>&1
 starttime=`date +%s`
 echo ${starttime} > ${LOGDIR}/start_time
-opam depext -uiv $PKG > ${LOGDIR}/stdout 2>${LOGDIR}/stderr
+jsontee -- opam depext -uiv $PKG > /tmp/log.json
+# TODO parse out exit code and set RES to that to signal error
+hash=`curl -s --data-binary @/tmp/log.json -X POST http://logs:8080/logs | jq -r .id`
+echo $hash > ${LOGDIR}/logs
 RES=$?
 endtime=`date +%s`
 echo ${endtime} > ${LOGDIR}/end_time
-difftime=$(($endtime - $starttime))
-echo $difftime > ${LOGDIR}/build_time
 exit $RES

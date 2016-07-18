74ca34529b5110dd80b0af2d7e4baf9130feb2a9
Anil Madhavapeddy <anil@recoil.org>
2016-07-18 19:27:43 +0100

    pull the right branch

--- command.sh
+++ command.sh
@@ -89,13 +89,13 @@ ginitpull() {
   log ginitpull
   git pull -q -r --no-commit --no-edit || (log aborting rebase; git rebase --abort; rsleep; return 1)
   git checkout --track -B state-$OPAM_REPO_REV
-  git pull -q -r --no-commit --no-edit || (log aborting rebase; git rebase --abort; rsleep; return 1)
+  git pull -q -r --no-commit --no-edit origin state-$OPAM_REPO_REV || (log aborting rebase; git rebase --abort; rsleep; return 1)
   return 0
 }
 
 gpull() {
   log gpull
-  git pull -q -r --no-commit --no-edit || (log aborting rebase; git rebase --abort; rsleep; return 1)
+  git pull -q -r --no-commit --no-edit origin state-$OPAM_REPO_REV || (log aborting rebase; git rebase --abort; rsleep; return 1)
   return 0
 }
 

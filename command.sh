0cd432e0e308daa981913d328ce5fc27846b1b77
Anil Madhavapeddy <anil@recoil.org>
2016-07-18 18:02:57 +0100

    comply with github's branch naming convention (no 40 char hex allowed)

--- command.sh
+++ command.sh
@@ -88,7 +88,7 @@ gcommit() {
 ginitpull() {
   log ginitpull
   git pull -q -r --no-commit --no-edit || (log aborting rebase; git rebase --abort; rsleep; return 1)
-  git checkout --track -B $OPAM_REPO_REV
+  git checkout --track -B state-$OPAM_REPO_REV
   return 0
 }
 
